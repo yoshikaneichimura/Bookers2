b95a5895264e4ed5d7bdb3925cedf0fa1bbc33ab
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-09-08 06:09:36 +0000

    [Add] Bookモデル Userモデル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramerters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_paramerters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
