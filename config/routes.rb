70a936e18019bc59c99f8d6a80aa491680bd8a0c
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-09-10 12:46:51 +0000

    [Add] homesコントローラ books コントローラ users コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  root 'homes#top'
+  get  'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
