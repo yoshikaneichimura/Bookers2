7309fa3ebf38cae8740696caf5ec431a7141b992
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-09-26 21:06:23 +0000

    [Update] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   root 'homes#top'
   get  'homes/about'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
