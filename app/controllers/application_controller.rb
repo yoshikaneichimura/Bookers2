8d5ac45a6d296d0c47a697545960ca59ce602215
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-10-31 21:10:18 +0000

    [Add] サインアップ機能　ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramerters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resouce)
+    root_path
+  end
+
   protected
 
   def configure_permitted_paramerters
