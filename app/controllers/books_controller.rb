ea3617b0fdf8715e22719e1706dca52b7738bd42
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-12-17 06:26:23 +0000

    [Add] サクセスメッセージ エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,15 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+       flash[:notice] = "You have created book successfully."
+       redirect_to books_path
+    else
+       @book = Book.new
+       @books = Book.all
+       @user = current_user
+       render :index
+    end
   end
 
   def show
@@ -24,8 +31,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully"
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
